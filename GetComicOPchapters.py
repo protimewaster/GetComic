e5c1513cab8c234043058fe2bed65c946ce1a7b3
protimewaster <singsinglee@live.hk>
2017-01-31 23:26:49 +0800

    Create GetComicOPchapters.py

---
+++ GetComicOPchapters.py
@@ -0,0 +1,26 @@
+import requests
+import re
+import urllib.request
+
+for chapter in range(852,853): #The starting and ending nos of the chapters
+	chapterno = str(chapter)
+	print (chapterno)
+	jsurl = ("http://comic.sfacg.com/Utility/2/" + chapterno + '.js')
+	jstext = requests.get(jsurl).text
+  # print (jstext)
+	picpath = re.findall("Pic/OnlineComic\d/OnePiece/\d\d\d/\d\d\d_\d*.\w\w\w",jstext)
+
+	for i in picpath:
+	    # print (i)
+	    host = 'http://coldpic.sfacg.com/'
+	    filename = re.findall("\d\d\d_\d*", i)
+	    # print (name)
+
+	    for b in range(len(filename)):
+	        path = chapterno + "-" + filename[b] + ".jpg"
+	        # print (path)
+	        picurl = host + i
+	        print (picurl)
+	        data = urllib.request.urlretrieve(picurl, path)
+
+print ('Work Done')
