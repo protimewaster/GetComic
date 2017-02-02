c75a2e4e0c94e1a4e450100c812aff4569103408
protimewaster <singsinglee@live.hk>
2017-02-03 00:41:29 +0800

    Update GetPTnovel.py
    optimize the code and make the code more readable

--- GetPTnovel.py
+++ GetPTnovel.py
@@ -1,30 +1,34 @@
 from bs4 import BeautifulSoup
-import urllib.request
-import ssl
 import requests
+import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 url = 'http://www.piaotian.net/html/8/8216/'
-resp = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(resp, 'html.parser')
+resp = requests.get(url)
+resp.encoding = 'gb18030'
+index = BeautifulSoup(resp.text, 'html.parser')
+name = index.find('h1').text
+filename = name + ".txt"
+f = open(filename,'w', encoding = 'utf-8')
 
-f = open("WuDaoZongSi.txt",'w', encoding = 'utf-8')
-
-title = soup.find_all("li")
-for i in range(4,len(title)):
-    chtitle = title[i].find_all('a', href=True)
-    for t in chtitle:
-        a = t['href']
-        raw = 'http://www.piaotian.net/html/8/8216/'
-        churl = raw + a
-        b = t.text
-        f.write(b + '\n')
-        chrep = requests.get(churl)
+chapters = index.find_all("li")
+for chapterno in range(4,len(chapters)):
+    chaptertitles = chapters[chapterno].findAll('a', href=True)
+    for chapter in chaptertitles:
+        chaptertitle = chapter.text
+        f.write(chaptertitle + '\n')
+        
+        chapterpath = chapter['href']
+        host = 'http://www.piaotian.net/html/8/8216/'
+        chapterurl = host + chapterpath
+        chrep = requests.get(chapterurl)
         chrep.encoding = 'gb18030'
         chsoup = BeautifulSoup(chrep.text, 'html.parser')
-        for i in chsoup.find_all('br'):
-            f.write(i.text + '\n')
-        print (b)
+        for p in chsoup.find_all('br'):
+            f.write(p.text + '\n')
+            
+        print (chaptertitle)
+        
 f.close()
 
-print ("Export is done")
+print ("All chapters are saved.")
