7a973e9da72249f6118072d50abfc786c7137c8b
protimewaster <singsinglee@live.hk>
2017-02-04 00:14:04 +0800

    Update GetComicOPchapters.py
    optimize code and make code readable

--- GetComicOPchapters.py
+++ GetComicOPchapters.py
@@ -8,19 +8,17 @@ for chapter in range(852,853): #The starting and ending nos of the chapters
 	jsurl = ("http://comic.sfacg.com/Utility/2/" + chapterno + '.js')
 	jstext = requests.get(jsurl).text
 ##	print (jstext)
-	picpath = re.findall("Pic/OnlineComic\d/OnePiece/\d\d\d/\d\d\d_\d*.\w\w\w",jstext)
+	lines = re.findall(".*?", jstext)
+	count = 1
 
-	for i in picpath:
-	    # print (i)
+	for line in lines:
+	    picpath = re.findall("Pic/.*?/.*.\w\w\w", line)
 	    host = 'http://coldpic.sfacg.com/'
-	    filename = re.findall("\d\d\d_\d*", i)
-	    # print (name)
-
-	    for b in range(len(filename)):
-	        path = chapterno + "-" + filename[b] + ".jpg"
-	        # print (path)
-	        picurl = host + i
-	        print (picurl)
+		filenum = r"%03d" % count
+		
+	    for path in picpath:
+	        path = chapterno + "-" + filenum + ".jpg"
+	        picurl = host + path
 	        data = urllib.request.urlretrieve(picurl, path)
 
 print ('Work Done')
